97d0f726734adc81d2e88a0363b4c9263fe4ede8
heylokesh <lokeshbaslay03@gmail.com>
2021-04-19 17:06:54 +0530

    First Commit

---
+++ core-java-project-lok/src/com/lokesh/trg/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.lokesh.trg;
+
+import java.util.Date;
+
+/**
+ * 
+ * @author lokkumar7
+ *Hey
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello");
+System.out.println("today is"+ new Date());
+	}
+
+}
